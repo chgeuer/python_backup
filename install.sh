aff55ea36a8d21154537e11bc6077e120d55f204
Christian Geuer-Pollmann <chgeuer@microsoft.com>
2018-05-28 19:06:12 +0200

    a

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
 
 sudo python2.7 -m ensurepip --default-pip
 sudo python2.7 -m pip install --upgrade pip
-sudo python2.7 -m pip install --upgrade pid
+sudo python2.7 -m pip install azure-cli pid
+
+
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[azure-cli]\nname=Azure CLI\nbaseurl=https://packages.microsoft.com/yumrepos/azure-cli\nenabled=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/zypp/repos.d/azure-cli.repo'
+sudo zypper refresh
+sudo zypper install --from azure-cli -y azure-cli
