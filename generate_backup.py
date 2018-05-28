658e9dcdb13a1778f11fbcb77914fc6575f19810
Christian Geuer-Pollmann <chgeuer@microsoft.com>
2018-05-28 16:04:39 +0200

    a

--- generate_backup.py
+++ generate_backup.py
@@ -1,13 +1,16 @@
-#! /usr/bin/env python
+#!/usr/bin/env python
 
 import sys
 import os
+import glob
+import subprocess
 import hashlib
 import base64
 import argparse
 import platform
 import pid
 import time
+import json
 
 #print(os.path.abspath(sys.argv[0]))
 #txt = "Nobody inspects the spammish repetition"
@@ -18,7 +21,7 @@ def main():
     parser = arg_parser() 
     args = parser.parse_args()
     if args.backup_full:
-        main_backup_full()
+        main_backup_full(args.config_file)
     elif args.backup_transactions:
         try:
             with pid.PidFile(pidname='txbackup', piddir='.') as p:
@@ -30,9 +33,16 @@ def main():
     else:
         parser.print_help()
 
+def account_credentials_from_file(filename):
+    with open(filename, mode='rt') as file:
+        content = file.read()
+    j = (json.JSONDecoder()).decode(content)
+    return (j['account_name'], j['account_key'])
+
 def arg_parser():
-    # https://docs.python.org/2/howto/argparse.html
     parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--config-file", 
+                        help="the JSON config file")
     parser.add_argument("-f", "--backup-full", 
                         help="Perform full backup",
                         action="store_true")
@@ -43,14 +53,54 @@ def arg_parser():
                         help="Perform restore for date")
     return parser
 
-def main_backup_full():
-    print(os.listdir("."))
-    time.sleep(20)
+def main_backup_full(filename="config.json"):
+    account_name, account_key = account_credentials_from_file(filename)
+    print "Use " + account_name + " " + account_key
+
+    container_name = "foo"
+    # source = "/mnt/c/Users/chgeuer/Videos"
+    source = "/mnt/c/Users/chgeuer/Desktop/Python Quick Start for Linux System Administrators/3"
+    pattern = "*.py"
+    print(os.listdir(source))
+
+    content = subprocess.check_output(['az', 'storage', 'container', 'create', 
+                                       '--account-name', account_name, 
+                                       '--account-key', account_key, 
+                                       '--name', container_name, 
+                                       '--output', 'json'])
+    created = (json.JSONDecoder()).decode(content)['created']
+    if (created):
+        print("Created container {}".format(container_name))
+    else:
+        print("container {} existed".format(container_name))
+
+    for filename in glob.glob1(dirname=source, pattern=pattern):
+        file = os.path.join(source, filename)
+        print("Upload {}".format(filename))
+        content = subprocess.check_output(['az', 'storage', 'blob', 'upload', 
+                                        '--account-name', account_name, 
+                                        '--account-key', account_key, 
+                                        '--max-connections', '2', 
+                                        '--type', 'block', 
+                                        '--file', file,
+                                        '--container-name', container_name, 
+                                        '--name', filename,
+                                        '--no-progress', 
+                                        '--output', 'json'])
+        j = (json.JSONDecoder()).decode(content)
+        print(j)
 
 def main_backup_transactions():
     print "Perform transactional backup"
+    print(os.listdir("."))
+    time.sleep(20)
 
 def main_restore(restore_point):
     print "Perform restore for restore point \"{}\"".format(restore_point)
+    with open('storage.json', mode='rt') as file:
+        content = file.read()
+    j = (json.JSONDecoder()).decode(content)
+    print(j)
 
-main()
+if __name__ == '__main__':
+    main()
