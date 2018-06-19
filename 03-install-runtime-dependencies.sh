2d9b7a132ebf4e1076181400e51e22e1fb0961f9
Christian Geuer-Pollmann <chgeuer@microsoft.com>
2018-06-19 15:39:25 +0200

    Remove old dependency install

--- 03-install-runtime-dependencies.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo python2.7 -m ensurepip --default-pip
-sudo python2.7 -m pip install --upgrade pip
-sudo python2.7 -m pip install -r requirements.txt
-
-# sudo python2.7 -m pip install pid azure-storage-blob
