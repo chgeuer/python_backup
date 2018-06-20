00cea11e5004f5b899cf4e09fd4133408b30a258
Christian Geuer-Pollmann <chgeuer@microsoft.com>
2018-06-20 14:29:12 +0200

    a

--- 00-ase-install.sh
+++ 00-ase-install.sh
@@ -2,18 +2,14 @@
 
 export SID="JLD"
 
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-NC='\033[0m' # No Color
-
 # Source: https://www.sap.com/cmp/syb/crm-xu15-int-asexprdm/index.html
 wget http://d1cuw2q49dpd0p.cloudfront.net/ASE16/Linux16SP03/ASE_Suite.linuxamd64.tgz
 mkdir ase_setup_src && cd ase_setup_src && tar xvfz ../ASE_Suite.linuxamd64.tgz
 
-echo "${RED}Make the folloging choices during installation:${NC}" 
-echo "${RED}Default Install Folder:             ${GREEN}/sybase/${SID} ${NC}"
-echo "${RED}Choose Install Set:                 ${GREEN}1 - Typical${NC}"
-echo "${RED}Software License Type Selection:    ${GREEN}2 - Install Express Edition of SAP Adaptive Server Enterprise${NC}"
+echo "$(tput setaf 1)Make the folloging choices during installation:$(tput sgr 0)" 
+echo "$(tput setaf 1)Default Install Folder:             $(tput setab 7)/sybase/${SID} $(tput sgr 0)"
+echo "$(tput setaf 1)Choose Install Set:                 $(tput setab 7)1 - Typical$(tput sgr 0)"
+echo "$(tput setaf 1)Software License Type Selection:    $(tput setab 7)2 - Install Express Edition of SAP Adaptive Server Enterprise$(tput sgr 0)"
 
 sudo ./setup.bin
 cd ..
