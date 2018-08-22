48dc3eaa7ba282aff3d5133b0e336e80034e2c7d
Christian Geuer-Pollmann <chgeuer@microsoft.com>
2018-08-22 16:53:27 +0200

    Include Windows download

--- 00-ase-install.sh
+++ 00-ase-install.sh
@@ -4,6 +4,7 @@ export SID="JLD"
 
 # Source: https://www.sap.com/cmp/syb/crm-xu15-int-asexprdm/index.html
 wget http://d1cuw2q49dpd0p.cloudfront.net/ASE16/Linux16SP03/ASE_Suite.linuxamd64.tgz
+# http://d1cuw2q49dpd0p.cloudfront.net/ASE16/Windows16SP03/ASE_Suite.winx64.zip
 mkdir ase_setup_src && cd ase_setup_src && tar xvfz ../ASE_Suite.linuxamd64.tgz
 
 echo "$(tput setaf 1)Make the folloging choices during installation:$(tput sgr 0)" 
