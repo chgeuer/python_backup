0bedcfcb5efb78cbb43d57cfba4f9d3deb06b580
Christian Geuer-Pollmann <chgeuer@microsoft.com>
2019-06-12 13:26:57 +0200

    Bump version to 0.4.1, and fix UNIX line endings.

--- asebackupcli/__init__.py
+++ asebackupcli/__init__.py
@@ -8,7 +8,7 @@
 
 """Version information."""
 
-__version__ = '0.4.0'
+__version__ = '0.4.1'
 
 def version():
     """The script's version"""
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='asebackupcli',
-    version='0.4.0',
+    version='0.4.1',
     packages=['asebackupcli'],
     description="A backup utility for Sybase ASE databases into Azure blob storage",
     author="Dr. Christian Geuer-Pollmann",
