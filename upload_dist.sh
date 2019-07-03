c0910e58990f47163b13f089a15edaac4696900e
Christian Geuer-Pollmann <chgeuer@microsoft.com>
2019-07-03 08:54:15 +0200

    No upload

--- upload_dist.sh
+++ upload_dist.sh
@@ -4,8 +4,8 @@ dest="asebackupcli-$(python setup.py --version).tar.gz"
 
 python setup.py sdist
 
-az storage blob upload \
-	--account-name "$(cat .azurestorageaccountname)" \
-	--account-key "$(cat .azurestorageaccountkey)" \
-	--container-name "software" \
-	--name "${dest}" --file "dist/${dest}"
+#az storage blob upload \
+#	--account-name "$(cat .azurestorageaccountname)" \
+#	--account-key "$(cat .azurestorageaccountkey)" \
+#	--container-name "software" \
+#	--name "${dest}" --file "dist/${dest}"
